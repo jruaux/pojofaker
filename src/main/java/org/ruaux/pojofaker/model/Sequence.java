108bbd90cb9bd34000c58ea8f17cc6183b61afe8
jruaux <jruaux@jrx.home.ruaux.org>
2018-09-04 21:41:07 -0700

    added sequence

--- src/main/java/org/ruaux/pojofaker/model/Sequence.java
+++ src/main/java/org/ruaux/pojofaker/model/Sequence.java
@@ -8,19 +8,17 @@ public class Sequence {
 	private Map<String, Long> sequences = new HashMap<String, Long>();
 
 	public long next(String key) {
-		long result = sequences.getOrDefault(key, 0L);
+		long result = sequences.getOrDefault(key, 1L);
 		sequences.put(key, result + 1);
 		return result;
 	}
 
-	public long next(String key, int digits, int interval) {
-		long next = next(key) / interval;
-		return (next % (int) Math.pow(10, digits)) + 1;
+	public long next(String key, long max) {
+		return next(key) % max;
 	}
 
-	public String next(String key, int digits, int interval, String format) {
-		long next = next(key, digits, interval);
-		return String.format(format, next);
+	public String next(String key, long max, String format) {
+		return String.format(format, next(key, max));
 	}
 
 }
