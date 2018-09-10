156f05a576ea1e80bbc92e22869cd83d914101ac
jruaux <jruaux@jrx.home.ruaux.org>
2018-09-10 08:24:55 -0700

    added start for sequence

--- src/main/java/org/ruaux/pojofaker/model/Sequence.java
+++ src/main/java/org/ruaux/pojofaker/model/Sequence.java
@@ -7,18 +7,18 @@ public class Sequence {
 
 	private Map<String, Long> sequences = new HashMap<String, Long>();
 
-	public long next(String key) {
-		long result = sequences.getOrDefault(key, 1L);
+	public long next(String key, long start) {
+		long result = sequences.getOrDefault(key, start);
 		sequences.put(key, result + 1);
 		return result;
 	}
 
-	public long next(String key, long max) {
-		return next(key) % max;
+	public long next(String key, long start, long end) {
+		return next(key, start) % end;
 	}
 
-	public String next(String key, long max, String format) {
-		return String.format(format, next(key, max));
+	public String next(String key, long start, long end, String format) {
+		return String.format(format, next(key, start, end));
 	}
 
 }
